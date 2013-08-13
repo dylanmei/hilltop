5bc7f3d0f7d2785b09fbce45e9821b3e2159eff2
dylanmei <dylanmei@gmail.com>
2013-08-13 11:37:09 -0700

    Skip buildprofile when showing non-Originating workflows

--- src/main/groovy/hilltop/WorkflowCommands.groovy
+++ src/main/groovy/hilltop/WorkflowCommands.groovy
@@ -16,28 +16,29 @@ class WorkflowCommands extends AnthillCommands {
       def (project, workflow) = get_workflow_or_complain(projectName, workflowName)
 
       def hint = workflow.isOriginating() ? '*' : ''
-      println "${project.getName()}-${workflow.getName()}$hint"
-
-
+      println "${workflow.getName()}$hint"
+      println "Project\t\t${project.getName()}"
       if (workflow.getDescription())
-        println "Description\t${project.getDescription()}"
+        println "Description\t${workflow.getDescription()}"
 
       def definition = workflow.getWorkflowDefinition()
       def lifecycleModel = definition.getLifeCycleModel()
       println "Lifecycle\t${lifecycleModel.getName()}"
 
-      def buildProfile = workflow.getBuildProfile()
-      def sourceConfig = buildProfile.getSourceConfig()
+      if (workflow.isOriginating()) {
+        def buildProfile = workflow.getBuildProfile()
+        def sourceConfig = buildProfile.getSourceConfig()
 
-      println "Source Config\t${definition.getSourceConfigTypeDisplayName()}"
-      if (sourceConfig.metaClass.respondsTo(sourceConfig, 'getRepositoryName'))
-        println "Repository Name\t${sourceConfig.getRepositoryName()}"
-      if (sourceConfig.metaClass.respondsTo(sourceConfig, 'getWorkspaceName'))
-        println "Workspace Name\t${sourceConfig.getWorkspaceName()}"
-      if (sourceConfig.metaClass.respondsTo(sourceConfig, 'getRevision'))
-        println "Repository Rev\t${sourceConfig.  getRevision()}"
-      if (sourceConfig.metaClass.respondsTo(sourceConfig, 'getRepositoryUrl'))
-        println "Repository URL\t${sourceConfig.getRepositoryUrl()}"
+        println "Source Config\t${definition.getSourceConfigTypeDisplayName()}"
+        if (sourceConfig.metaClass.respondsTo(sourceConfig, 'getRepositoryName'))
+          println "Repository Name\t${sourceConfig.getRepositoryName()}"
+        if (sourceConfig.metaClass.respondsTo(sourceConfig, 'getWorkspaceName'))
+          println "Workspace Name\t${sourceConfig.getWorkspaceName()}"
+        if (sourceConfig.metaClass.respondsTo(sourceConfig, 'getRevision'))
+          println "Repository Rev\t${sourceConfig.  getRevision()}"
+        if (sourceConfig.metaClass.respondsTo(sourceConfig, 'getRepositoryUrl'))
+          println "Repository URL\t${sourceConfig.getRepositoryUrl()}"
+      }
     }
   }
 
